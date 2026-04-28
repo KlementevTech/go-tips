8f2782c61a058c5a42900701f388abba3ccbf7d4
KlementevTech <andre.itskill@gmail.com>
2026-04-28 19:12:01 +0300

    feat(perf): add k6 perf testing

--- README.md
+++ README.md
@@ -10,26 +10,26 @@
 grpcurl -plaintext \
     -d '{"id": "019d5da1-46dd-7b0d-82e5-49345ac87e78", "name": "Turbo21"}' \
     127.0.0.1:50051 \
-    catalog.v1.PcPartStoreService/CreatePcPart
+    catalog.v1.PcPartService/CreatePcPart
 ```
 
 ```shell
 grpcurl -plaintext \
     -d '{"id": "019d5da1-46dd-7b0d-82e5-49345ac87e78"}' \
     127.0.0.1:50051 \
-    catalog.v1.PcPartStoreService/GetPcPart
+    catalog.v1.PcPartService/GetPcPart
 ```
 
 ```shell
 grpcurl -plaintext \
     -d '{"pc_part": {"id": "019d5da1-46dd-7b0d-82e5-49345ac87e78", "version": "1", "name": "turbo2"}}' \
     127.0.0.1:50051 \
-    catalog.v1.PcPartStoreService/UpdatePcPart
+    catalog.v1.PcPartService/UpdatePcPart
 ```
 
 ```shell
 grpcurl -plaintext \
     -d '{"id": "019d5da1-46dd-7b0d-82e5-49345ac87e78", "version": "2"}' \
     127.0.0.1:50051 \
-    catalog.v1.PcPartStoreService/DeletePcPart
+    catalog.v1.PcPartService/DeletePcPart
 ```
---
+++ test/k6/scripts/test.js
@@ -0,0 +1,46 @@
+import grpc from 'k6/net/grpc';
+import { check } from 'k6';
+
+const GRPC_ADDRESS = __ENV.GRPC_ADDRESS || "app:50051";
+const DURATION = __ENV.DURATION || "1m";
+const VUS = parseInt(__ENV.VUS) || 500;
+
+const client = new grpc.Client();
+client.load(['/proto'], 'catalog/v1/pcpart.proto');
+client.load(['/proto'], 'catalog/v1/pcpart_service.proto');
+
+export const options = {
+    stages: [
+        { duration: '30s', target: 10 },
+        { duration: DURATION, target: VUS },
+        { duration: '30s', target: 0 },
+    ],
+    thresholds: {
+        'checks': ['rate>0.9'],
+        'grpc_req_duration': ['p(95)<500'],
+    },
+};
+
+// Переменная внутри VU (не глобальная для всех, а своя у каждого VU)
+let connected = false;
+
+export default () => {
+    // Если этот конкретный VU еще не подключен — подключаемся
+    if (!connected) {
+        client.connect(GRPC_ADDRESS, {
+            plaintext: true,
+            timeout: '5s' // Даем время на установку соединения
+        });
+        connected = true;
+    }
+
+    const response = client.invoke('catalog.v1.PcPartService/GetPcPart', {
+        "id": "019d5da1-46dd-7b0d-82e5-49345ac87e79"
+    });
+
+    check(response, {
+        'status is OK': (r) => r && r.status === grpc.StatusOK,
+    });
+
+    // МЫ НЕ ЗАКРЫВАЕМ СОЕДИНЕНИЕ (client.close() удален)
+};
